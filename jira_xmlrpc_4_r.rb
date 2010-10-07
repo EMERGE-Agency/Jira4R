f12ec0fcb1a43bda6f6e715dd4028758d8832948
Justin Barry <barrium@Macintosh.local>
2010-10-07 11:50:59 -0700

    added CallerChain class to get the calling method to simplify call_remote_method.

--- jira_xmlrpc_4_r.rb
+++ jira_xmlrpc_4_r.rb
@@ -1,11 +1,37 @@
 require 'xmlrpc/client'
 
 module JiraXMLRPC4R
+  #returns the name of the calling method. Will be used to find files named for tests.
+	class CallChain
+	  def self.caller_method(depth=1)
+		parse_caller(caller(depth+1).first).last
+	  end
+
+	  private
+	  #Stolen from ActionMailer, where this was used but was not made reusable
+	  def self.parse_caller(at)
+		if /^(.+?):(\d+)(?::in `(.*)')?/ =~ at
+		  file   = Regexp.last_match[1]
+		  line   = Regexp.last_match[2].to_i
+		  method = Regexp.last_match[3]
+		  [file, line, method]
+		end
+	  end
+	end
+  
   class JiraTool
     
     def initialize(host=nil,path=nil,port=nil,username=nil,password=nil, default_remote_object = "jira1")
       @server = XMLRPC::Client.new(host, path, port)
       @default_remote_object = default_remote_object
+    end
+    
+    def login(username, password)
+      @token = call_remote_method()
+    end
+    
+    def call_remote_method(*args)
+      @service.call("#{@default_remote_object}.#{CallerChain.caller_method}", *args)
     end
     
     #boolean	addComment(java.lang.String token, java.lang.String issueKey, java.lang.String comment) 
@@ -48,13 +74,8 @@ module JiraXMLRPC4R
     
     #java.lang.String	login(java.lang.String username, java.lang.String password) 
     #Logs the user into JIRA.
-    def login(username, password)
-      @token = @service.call("#{@default_remote_object}.login", username, password)
-    end
-    
-    #boolean	logout(java.lang.String token) 
-    #Logs the user out of JIRA
-    
+
+        
     #java.util.Hashtable	updateIssue(java.lang.String token, java.lang.String issueKey, java.util.Hashtable fieldValues) 
     #Updates an issue in JIRA from a Hashtable object.
     
@@ -80,5 +101,8 @@ module JiraXMLRPC4R
     
     #java.util.Vector	getSubTaskIssueTypes(java.lang.String token) 
     #Returns all visible subtask issue types in the system
+    
+    #boolean	logout(java.lang.String token) 
+    #Logs the user out of JIRA
   end
 end
