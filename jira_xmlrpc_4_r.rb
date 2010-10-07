ba725bd9f30d15a6ecfc63b0d66dfd3c71e20382
Justin Barry <barrium@Macintosh.local>
2010-10-07 13:20:30 -0700

    removed getIssue and logout methods. They will be created dynamically with other methods of similar signature.

--- jira_xmlrpc_4_r.rb
+++ jira_xmlrpc_4_r.rb
@@ -1,6 +1,97 @@
 require 'xmlrpc/client'
 
 module JiraXMLRPC4R
+  class JiraTool
+    
+    def initialize(host=nil,path=nil,port=nil,username=nil,password=nil, default_remote_object = "jira1")
+      @service = XMLRPC::Client.new(host, path, port)
+      @default_remote_object = default_remote_object
+    end
+    
+    #string	login(string username, string password) 
+    #Logs the user into JIRA.
+    def login(username, password)
+      @token = call_remote_method(username, password)
+    end
+    
+    #boolean	addComment(string token, string issueKey, string comment) 
+    #Adds a comment to an issue
+    def add_comment(issue_key, comment)
+      call_remote_method(issue_key, comment)      
+    end
+    
+    #hash	createIssue(string token, hash rIssueStruct) 
+    #Creates an issue in JIRA from a Hashtable object.
+    
+    #hash	updateIssue(string token, string issueKey, hash fieldValues) 
+    #Updates an issue in JIRA from a Hashtable object.
+    
+    #array	getIssuesFromTextSearchWithProject(string token, array projectKeys, string searchTerms, int maxNumResults) 
+    #Find issues using a free text search, limited to certain projects
+    
+    #TODO construct these from array
+      #array	getComments(string token, string issueKey) 
+      #Returns all comments associated with the issue
+    
+      #array	getComponents(string token, string projectKey) 
+      #Returns all components available in the specified project
+    
+      #array	getIssuesFromFilter(string token, string filterId) 
+      #Executes a saved filter
+      
+      #hash	getUser(string token, string username) 
+      #Returns a user's information given a username
+
+      #array	getIssueTypesForProject(string token, string projectId) 
+      #Returns all visible (non-sub task) issue types for the specified project id
+
+      #array	getSubTaskIssueTypesForProject(string token, string projectId) 
+      #Returns all visible sub task issue types for the specified project id.
+
+      #array	getIssuesFromTextSearch(string token, string searchTerms) 
+      #Find issues using a free text search
+
+      #array	getVersions(string token, string projectKey) 
+      #Returns all versions available in the specified project
+      
+      #hash	getIssue(string token, string issueKey) 
+      #Gets an issue from a given issue key.
+    
+    #TODO: Construct these from array.
+      #array	getFavouriteFilters(string token) 
+      #Gets all favourite filters available for the currently logged in user
+    
+      #array	getIssueTypes(string token) 
+      #Returns all visible issue types in the system
+    
+      #array	getPriorities(string token) 
+      #Returns all priorities in the system
+    
+      #array	getProjectsNoSchemes(string token) 
+      #Returns a list of projects available to the user
+    
+      #array	getResolutions(string token) 
+      #Returns all resolutions in the system
+    
+      #hash	getServerInfo(string token) 
+      #Returns the Server information such as baseUrl, version, edition, buildDate, buildNumber.
+    
+      #array	getStatuses(string token) 
+      #Returns all statuses in the system
+    
+      #array	getSubTaskIssueTypes(string token) 
+      #Returns all visible subtask issue types in the system
+      
+      #boolean	logout(string token) 
+      #Logs the user out of JIRA
+    
+    private
+      def call_remote_method(*args)
+        args.insert(0, @token) if @token
+        @service.call("#{@default_remote_object}.#{Utils.camelized_caller_method}", *args)
+      end
+  end
+  
   #returns the name of the calling method. Will be used to find files named for tests.
 	class Utils
 	  def self.caller_method(depth=1)
@@ -31,97 +122,5 @@ module JiraXMLRPC4R
   		  [file, line, method]
   		end
 	  end
-	end
-
-  
-  class JiraTool
-    
-    def initialize(host=nil,path=nil,port=nil,username=nil,password=nil, default_remote_object = "jira1")
-      @service = XMLRPC::Client.new(host, path, port)
-      @default_remote_object = default_remote_object
-    end
-    
-    #string	login(string username, string password) 
-    #Logs the user into JIRA.
-    def login(username, password)
-      @token = call_remote_method(username, password)
-    end
-    
-    #hash	getIssue(string token, string issueKey) 
-    #Gets an issue from a given issue key.
-    def get_issue(issue_key)
-      call_remote_method(issue_key)
-    end
-    
-    #boolean	addComment(string token, string issueKey, string comment) 
-    #Adds a comment to an issue
-    def add_comment()
-      
-    end
-    
-    #hash	createIssue(string token, hash rIssueStruct) 
-    #Creates an issue in JIRA from a Hashtable object.
-    
-    #array	getComments(string token, string issueKey) 
-    #Returns all comments associated with the issue
-    
-    #array	getComponents(string token, string projectKey) 
-    #Returns all components available in the specified project
-    
-    #array	getIssuesFromFilter(string token, string filterId) 
-    #Executes a saved filter
-    
-    #array	getIssuesFromTextSearch(string token, string searchTerms) 
-    #Find issues using a free text search
-    
-    #array	getIssuesFromTextSearchWithProject(string token, array projectKeys, string searchTerms, int maxNumResults) 
-    #Find issues using a free text search, limited to certain projects
-
-    #array	getIssueTypesForProject(string token, string projectId) 
-    #Returns all visible (non-sub task) issue types for the specified project id
-    #array	getSubTaskIssueTypesForProject(string token, string projectId) 
-    #Returns all visible sub task issue types for the specified project id.
-    
-    #hash	getUser(string token, string username) 
-    #Returns a user's information given a username
-    
-    #array	getVersions(string token, string projectKey) 
-    #Returns all versions available in the specified project
-    
-    #hash	updateIssue(string token, string issueKey, hash fieldValues) 
-    #Updates an issue in JIRA from a Hashtable object.
-    
-    #TODO: Construct these from array.
-    #array	getFavouriteFilters(string token) 
-    #Gets all favourite filters available for the currently logged in user
-    
-    #array	getIssueTypes(string token) 
-    #Returns all visible issue types in the system
-    
-    #array	getPriorities(string token) 
-    #Returns all priorities in the system
-    
-    #array	getProjectsNoSchemes(string token) 
-    #Returns a list of projects available to the user
-    
-    #array	getResolutions(string token) 
-    #Returns all resolutions in the system
-    
-    #hash	getServerInfo(string token) 
-    #Returns the Server information such as baseUrl, version, edition, buildDate, buildNumber.
-    
-    #array	getStatuses(string token) 
-    #Returns all statuses in the system
-    
-    #array	getSubTaskIssueTypes(string token) 
-    #Returns all visible subtask issue types in the system
-    
-    #boolean	logout(string token) 
-    #Logs the user out of JIRA
-    private
-      def call_remote_method(*args)
-        args.insert(0, @token) if @token
-        @service.call("#{@default_remote_object}.#{Utils.camelized_caller_method}", *args)
-      end
-  end  
+	end 
 end
