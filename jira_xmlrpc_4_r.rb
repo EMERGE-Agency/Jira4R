fb602fe29920766ef317b3492bad8c63c4d6133d
Justin Barry <barrium@Macintosh.local>
2010-10-07 12:29:57 -0700

    added camelize method and changed caller chain class to utils class. converts from ruby method signatures to java

--- jira_xmlrpc_4_r.rb
+++ jira_xmlrpc_4_r.rb
@@ -2,11 +2,24 @@ require 'xmlrpc/client'
 
 module JiraXMLRPC4R
   #returns the name of the calling method. Will be used to find files named for tests.
-	class CallChain
+	class Utils
 	  def self.caller_method(depth=1)
 		parse_caller(caller(depth+1).first).last
 	  end
-
+    
+    def self.camelized_caller_method(depth=1)
+		  self.camelize(parse_caller(caller(depth+1).first).last)
+    end
+    
+    #From http://github.com/rails/rails/blob/master/activesupport/lib/active_support/inflector/methods.rb
+    def self.camelize(lower_case_and_underscored_word, first_letter_in_uppercase = false)
+      if first_letter_in_uppercase
+        lower_case_and_underscored_word.to_s.gsub(/\/(.?)/) { "::#{$1.upcase}" }.gsub(/(?:^|_)(.)/) { $1.upcase }
+      else
+        lower_case_and_underscored_word.to_s[0].chr.downcase + camelize(lower_case_and_underscored_word)[1..-1]
+      end
+    end
+    
 	  private
 	  #Stolen from ActionMailer, where this was used but was not made reusable
 	  def self.parse_caller(at)
@@ -18,91 +31,91 @@ module JiraXMLRPC4R
 		end
 	  end
 	end
+
   
   class JiraTool
     
     def initialize(host=nil,path=nil,port=nil,username=nil,password=nil, default_remote_object = "jira1")
-      @server = XMLRPC::Client.new(host, path, port)
+      @service = XMLRPC::Client.new(host, path, port)
       @default_remote_object = default_remote_object
     end
     
     def login(username, password)
-      @token = call_remote_method()
+      @token = call_remote_method(username, password)
     end
     
     def call_remote_method(*args)
-      @service.call("#{@default_remote_object}.#{CallerChain.caller_method}", *args)
+      args.insert(0, @token) if @token
+      @service.call("#{@default_remote_object}.#{Utils.camelized_caller_method}", *args)
     end
     
-    #boolean	addComment(java.lang.String token, java.lang.String issueKey, java.lang.String comment) 
+    #boolean	addComment(string token, string issueKey, string comment) 
     #Adds a comment to an issue
     
-    #java.util.Hashtable	createIssue(java.lang.String token, java.util.Hashtable rIssueStruct) 
+    #hash	createIssue(string token, hash rIssueStruct) 
     #Creates an issue in JIRA from a Hashtable object.
     
-    #java.util.Vector	getComments(java.lang.String token, java.lang.String issueKey) 
+    #array	getComments(string token, string issueKey) 
     #Returns all comments associated with the issue
     
-    #java.util.Vector	getComponents(java.lang.String token, java.lang.String projectKey) 
+    #array	getComponents(string token, string projectKey) 
     #Returns all components available in the specified project
     
-    #java.util.Vector	getFavouriteFilters(java.lang.String token) 
-    #Gets all favourite filters available for the currently logged in user
-    
-    #java.util.Hashtable	getIssue(java.lang.String token, java.lang.String issueKey) 
+    #hash	getIssue(string token, string issueKey) 
     #Gets an issue from a given issue key.
     
-    #java.util.Vector	getIssuesFromFilter(java.lang.String token, java.lang.String filterId) 
+    #array	getIssuesFromFilter(string token, string filterId) 
     #Executes a saved filter
     
-    #java.util.Vector	getIssuesFromTextSearch(java.lang.String token, java.lang.String searchTerms) 
+    #array	getIssuesFromTextSearch(string token, string searchTerms) 
     #Find issues using a free text search
     
-    #java.util.Vector	getIssuesFromTextSearchWithProject(java.lang.String token, java.util.Vector projectKeys, java.lang.String searchTerms, int maxNumResults) 
+    #array	getIssuesFromTextSearchWithProject(string token, array projectKeys, string searchTerms, int maxNumResults) 
     #Find issues using a free text search, limited to certain projects
 
-    #java.util.Vector	getIssueTypesForProject(java.lang.String token, java.lang.String projectId) 
+    #array	getIssueTypesForProject(string token, string projectId) 
     #Returns all visible (non-sub task) issue types for the specified project id
-    #java.util.Vector	getSubTaskIssueTypesForProject(java.lang.String token, java.lang.String projectId) 
+    #array	getSubTaskIssueTypesForProject(string token, string projectId) 
     #Returns all visible sub task issue types for the specified project id.
     
-    #java.util.Hashtable	getUser(java.lang.String token, java.lang.String username) 
+    #hash	getUser(string token, string username) 
     #Returns a user's information given a username
     
-    #java.util.Vector	getVersions(java.lang.String token, java.lang.String projectKey) 
+    #array	getVersions(string token, string projectKey) 
     #Returns all versions available in the specified project
     
-    #java.lang.String	login(java.lang.String username, java.lang.String password) 
+    #string	login(string username, string password) 
     #Logs the user into JIRA.
-
-        
-    #java.util.Hashtable	updateIssue(java.lang.String token, java.lang.String issueKey, java.util.Hashtable fieldValues) 
+    
+    #hash	updateIssue(string token, string issueKey, hash fieldValues) 
     #Updates an issue in JIRA from a Hashtable object.
     
-    
     #TODO: Construct these from array.
-    #java.util.Vector	getIssueTypes(java.lang.String token) 
+    #array	getFavouriteFilters(string token) 
+    #Gets all favourite filters available for the currently logged in user
+    
+    #array	getIssueTypes(string token) 
     #Returns all visible issue types in the system
     
-    #java.util.Vector	getPriorities(java.lang.String token) 
+    #array	getPriorities(string token) 
     #Returns all priorities in the system
     
-    #java.util.Vector	getProjectsNoSchemes(java.lang.String token) 
+    #array	getProjectsNoSchemes(string token) 
     #Returns a list of projects available to the user
     
-    #java.util.Vector	getResolutions(java.lang.String token) 
+    #array	getResolutions(string token) 
     #Returns all resolutions in the system
     
-    #java.util.Hashtable	getServerInfo(java.lang.String token) 
+    #hash	getServerInfo(string token) 
     #Returns the Server information such as baseUrl, version, edition, buildDate, buildNumber.
     
-    #java.util.Vector	getStatuses(java.lang.String token) 
+    #array	getStatuses(string token) 
     #Returns all statuses in the system
     
-    #java.util.Vector	getSubTaskIssueTypes(java.lang.String token) 
+    #array	getSubTaskIssueTypes(string token) 
     #Returns all visible subtask issue types in the system
     
-    #boolean	logout(java.lang.String token) 
+    #boolean	logout(string token) 
     #Logs the user out of JIRA
-  end
+  end  
 end
